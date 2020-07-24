6dd9731019235cef7906a1fb97ecafb438aa13e8
tonogeneral <tonogeneral@gmail.com>
2020-07-24 14:58:07 -0400

    Se renombra archivo

---
+++ calificacion_notas.py
@@ -0,0 +1,19 @@
+# Archivo que calcula la calilficación obtenida
+
+nota=input("Ingrese nota:")
+try:
+    fn=float(nota)
+except:
+    print("Ingrese valor entre 0.0 y 1.0")
+    quit()
+if fn >= 0.9:
+    calif="A"
+elif fn < 0.9 and fn >= 0.8:
+    calif="B"
+elif fn < 0.8 and fn >= 0.7:
+    calif="C"
+elif fn < 0.7 and fn >= 0.6:
+    calif="D"
+elif fn < 0.6:
+    calif="F"
+print(calif)
