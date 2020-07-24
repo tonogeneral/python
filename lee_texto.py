388cef6717f3efa692dfc576b5d24976fa396613
tonogeneral <tonogeneral@gmail.com>
2020-07-24 15:06:05 -0400

    Cambia nombre a archivo

---
+++ lee_texto.py
@@ -0,0 +1,20 @@
+#correo=open("mbox-short.txt","r")
+
+#BUSCAR TODOS LOS DOMINIOS DE LA LISTA DE CORREOS
+
+#count = 0
+correo=open('mbox-short.txt')
+count=0
+for line in correo:
+    print(line)
+
+print (count)
+    #print(arroba)
+    #count=count+1
+
+#print(count)
+
+  # print(line.strip())
+ #  count = count + 1
+
+#print(correo.read())
