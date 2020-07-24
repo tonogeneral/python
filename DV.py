7247119c914cc1e3e78c9eb60625b818e79809d9
tonogeneral <tonogeneral@gmail.com>
2020-07-24 13:41:56 -0400

    Este es un mensaje

---
+++ DV.py
@@ -0,0 +1,44 @@
+# Use the file name mbox-short.txt as the file name
+rut = input("Ingrese un rut valido: ")
+try:
+    num=rut.replace(".",'') #Quitamos puntos
+    numero=num.find('-') #Encontramos posicion guion
+    valor=num[0:numero] #Separamos valor
+    ivalor=int(valor) #transformamos a entero
+    svalor=str(ivalor)#seteamos como string ese valor
+    digito=num[numero+1:numero+2] # guardamos el digito
+
+except:
+    print("RUT CON FORMATO INVALIDO")
+    quit()
+
+#reversamos valor numerico
+def reverse(svalor):
+    rev = ''
+    for i in range(len(svalor), 0, -1):
+        rev += svalor[i-1]
+    return rev
+
+trabajo=reverse(valor)
+suma=0
+mult=2
+for pos in trabajo:
+    if mult <=7:
+        suma=(int(pos)*mult)+suma
+        mult=mult+1
+        print(rut,reverse(svalor),suma)
+    elif mult >7:
+        mult=2
+        suma=(int(pos)*mult)+suma
+        mult=mult+1
+        #print(rut,reverse(svalor),suma)
+modulo=suma%11
+DV=11-modulo
+if DV <10:
+    DV=str(DV)
+else:
+    DV='K'
+if (DV==digito.upper()):
+    print("RUT CORRECTO")
+else:
+    print("RUT INCORRECTO")
